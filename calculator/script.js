02ec882dee42844e3ac7f788aea0e09dc757e943
ft-frank <frank.imagination@gmail.com>
2025-12-05 16:58:57 +1100

    Completed calculator with extra points!

--- calculator/script.js
+++ calculator/script.js
@@ -8,9 +8,19 @@ var buttons = document.querySelectorAll("button")
 
 var clear = document.querySelector("#clear")
 
-var operating = undefined
+var addition = document.querySelector("#add")
+var subtraction = document.querySelector("#subtract")
+var division = document.querySelector("#divide")
+var multiplication = document.querySelector("#multiply")
+var equal = document.querySelector("#equal")
 
-var calculating = undefined
+var operating = undefined // Operating is true if an operation has been selected
+
+var calculating = undefined // Calculating is true if a number has been selected
+
+var continu = undefined // coninuing calculations without needing to press equal
+
+var decimal = document.querySelector("#decimal")
 
 var numbersSelected = []
 var operationsSelected = []
@@ -23,18 +33,62 @@ var number2 = undefined
 //4. if an operation or equals is clicked, then it performs the operation. 
 
 
+document.addEventListener('keydown', (event)=> {
+    console.log(event.key)
+    if (event.key == "Backspace") {
+        if (screen.textContent == 0){
+            ;
+        }
+        else if (screen.textContent.length == 1) {
+            screen.textContent = "0"
+        }
+        else
+        {
+        screen.textContent = screen.textContent.slice(0,screen.textContent.length -1    ) }
+    }
+
+    if (event.key == "+") {
+        addition.click()
+
+    }
+    if (event.key == "-") {
+        subtraction.click()
+    }
+    if (event.key == "*") {
+        multiplication.click()
+    }
+
+    if (event.key == "/") {
+        division.click()
+    }
+
+    if (event.key == "Enter" || event.key == "=") {
+        equal.click()
+    }
+
+
+
+
+
+
+
+
+
+ })
+
 buttons.forEach((button) => 
-    button.addEventListener('click', () => {
-
-        if (calculating) {
-            clear.textContent = "CE"
-
+    button.addEventListener('click', () => {    
+        
+        if ((screen.textContent == 0)){
+            clear.textContent = "AC"
         }
         else {
-            clear.textContent  = "AC"
-
+            clear.textCOntent = "CE"
         }
-
+    
+        
+        
+        operations.forEach ((operator) => operator.removeAttribute("style"));   
 } ))
 
 
@@ -43,18 +97,50 @@ clear.addEventListener('click', ()=> {
         numbersSelected = []
         operationsSelected = []
         clear.textContent = "AC"
+        operating = undefined 
+        calculating = undefined 
+        continu = undefined 
     }
     else {
         screen.textContent = "0";
         numbersSelected.shift()
 
 
+
     }
+
+        if (clear.textContent == "CE") {
+
+            clear.textContent = "AC"
+        }
+
 
     }
 
 
 )
+
+decimal.addEventListener('click', () =>{
+    if (screen.textContent == 0) {
+        ;
+    }
+        
+    else if (!(String(screen.textContent).includes("."))) {
+        screen.textContent = screen.textContent + "." 
+    }}
+
+) 
+
+
+numbers.forEach((number) => {
+        document.addEventListener('keydown', (event) => {
+            if (event.key == number.textContent && screen.textContent.length < 9) {
+                number.click()
+            }
+                
+            }
+        ) 
+    })
 
 
 numbers.forEach((number) => 
@@ -70,6 +156,8 @@ numbers.forEach((number) =>
         else {
             screen.textContent = screen.textContent + number.textContent
         }
+
+
         operating = false   
         calculating = true
     } )
@@ -80,26 +168,51 @@ numbers.forEach((number) =>
 
 operations.forEach((operator) => {
 
+
     operator.addEventListener('click', () => {
+
+        if (continu == true && calculating ==false && operating == true) {
+            ;
+        }
+        else if (continu == true) {
+            answer()
+        }
+    
+        continu = true
 
         operating = true
 
         calculating = false
 
-        console.log(operator.textContent)
-
         operations.forEach((op) => op.removeAttribute("style"));
         operator.style.backgroundColor = "white";
 
         operationsSelected.unshift(operator.textContent);
-        numbersSelected.unshift(parseInt(screen.textContent));
+        numbersSelected.unshift(parseFloat(screen.textContent));
 
     });
 
 });
 
-equal = document.querySelector("#equal") 
-equal.addEventListener('click', () => {console.log(numbersSelected);console.log(operationsSelected);calculating = false; numbersSelected.unshift(parseInt(screen.textContent)); screen.textContent = roundToDecimal(operate(numbersSelected[1], numbersSelected[0], operationsSelected[0]), 9)})
+equal.addEventListener('click', () => {
+    answer()
+}
+)
+
+
+function answer () {
+    operating = true;
+    calculating = false; 
+    continu = false;
+    numbersSelected.unshift(parseFloat(screen.textContent));
+    if (numbersSelected[1] == undefined) {
+        number1 = numbersSelected[0]
+    }
+    number1 = numbersSelected[1]
+    number2 = numbersSelected[0]
+    operation = operationsSelected[0]
+    screen.textContent = roundToDecimal(operate(number1, number2, operation), 9)}
+
 
 
 function roundToDecimal(num, decimalPlaces) {
